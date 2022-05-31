b398d6e32ce15c5da15a7269ae31ee9e5bf28061
Jake Donham <jaked@github.com>
2022-05-31 13:47:31 -0700

    drop unused useRef

--- src/components/local-block.tsx
+++ src/components/local-block.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 // @ts-ignore
 import loadable from "@loadable/component";
 import {
