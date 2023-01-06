3b23d4d906c42ae7b3f5244b228f6a7841da4ace
Jake Donham <jaked@github.com>
2023-01-05 16:17:29 -0800

    option for Vite to use https

--- scripts/start.js
+++ scripts/start.js
@@ -1,10 +1,7 @@
-const fs = require("fs");
 const chalk = require("chalk");
-const express = require("express");
 const { createServer } = require("vite");
 const getViteConfigDev = require("./config/vite.config.dev");
-const parseGitConfig = require("parse-git-config");
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(2));
 
 process.env.BABEL_ENV = "development";
 process.env.NODE_ENV = "development";
@@ -13,35 +10,22 @@ require("./config/env");
 
 const main = async () => {
   const port = argv.port || process.env.PORT || 4000;
-  const app = express();
-
-  const vite = await createServer(getViteConfigDev(port))
-
-  app.get("/blocks.config.json", (req, res) => {
-    const json = fs.readFileSync("./blocks.config.json");
-    const obj = JSON.parse(json);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Private-Network", "true");
-    res.json(obj);
-  });
-
-  app.get("/git.config.json", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Private-Network", "true");
-    res.json(parseGitConfig.sync());
-  });
-
-  app.use(vite.middlewares);
+  const https = Boolean(argv.https || process.env.HTTPS);
+  const devServer = await createServer(getViteConfigDev(port, https));
 
   console.log(
-    chalk.cyan(`Starting the development server at http://localhost:${port}`)
+    chalk.cyan(
+      `Starting the development server at http${
+        https ? "s" : ""
+      }://localhost:${port}`
+    )
   );
-  app.listen(port);
+  await devServer.listen();
 
   if (process.env.CI !== "true") {
     // Gracefully exit when stdin ends
     process.stdin.on("end", function () {
-      app.close();
+      devServer.close();
       process.exit();
     });
   }
